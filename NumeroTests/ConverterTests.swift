3c12b6b3648c215f29803914a4621808e1d761cb
Abdulhalik <abdlhlkkrkmz@gmail.com>
2021-10-05 16:35:35 +0300

    ConverterTests and Init Travis

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -26,4 +26,25 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+    var converter: Converter!
+    
+    override func setUp() {
+        super.setUp()
+        converter = Converter()
+    }
+    
+    override func tearDown() {
+        converter = nil
+        super.tearDown()
+    }
+    
+    func testConversionForOne() {
+        let result = converter.convert(1)
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect.")
+    }
+}
 
